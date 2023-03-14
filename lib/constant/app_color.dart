514cf092b7cea2814bda69e759995ffa058309c9
Adebayo Adesina <adebayoadesina789@gmail.com>
2023-03-14 18:42:19 +0100

    flutter chatGTP.

---
+++ lib/constant/app_color.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/animation.dart';
+
+class AppColor {
+  AppColor._();
+
+  static Color scaffoldBackgroundColor = const Color(0xff343541);
+  static Color cardColor = const Color(0xff444654);
+}
